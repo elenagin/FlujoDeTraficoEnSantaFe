1aef67ac9dcdc96628dbfde8df39a6ed2d8096ad
Lenn <lennginzan@gmail.com>
2018-11-12 13:55:35 -0600

    all windows working

--- ventanas.c
+++ ventanas.c
@@ -143,7 +143,7 @@ void VentanaInstrucciones(GtkWidget *widget, gpointer data)
 void VentanaPrincipal(GtkWidget *widget, gpointer data)
 {
   int i=0;
-  GtkWidget *CajaGrande, *botonOK, *Tool;
+  GtkWidget *CajaGrande, *botonOK, *Tool, *hbox1, *hbox2;
   ptrWidgets Widgets=(ptrWidgets)data;
   GtkToolItem *ToolSalir, *Separador, *ToolAyuda, *ToolAcercaDe;
   
@@ -158,10 +158,10 @@ void VentanaPrincipal(GtkWidget *widget, gpointer data)
   gtk_container_border_width(GTK_CONTAINER(Widgets->SVentanas->VenP),5);
 
   //Creación de cajas
-  CajaGrande=gtk_vbox_new(FALSE,5);//nueva caja vertical
-  gtk_container_add(GTK_CONTAINER(Widgets->SVentanas->VenP),CajaGrande);//agregamos la caja vertical de jugador a la ventana
-  for(i=0;i<2;i++)//creacion de cajas horizontales
-    Widgets->SOpciones->CH[i]=gtk_hbox_new(FALSE,3);
+  CajaGrande=gtk_vbox_new(FALSE,0);//nueva caja vertical
+  hbox1=gtk_hbox_new(FALSE,0);
+  hbox2=gtk_hbox_new(FALSE, 0);
+  gtk_container_add(GTK_CONTAINER(Widgets->SVentanas->VenP), CajaGrande);//agregamos la caja vertical de jugador a la ventana
   
   //Barra de accesos directos
   Tool=gtk_toolbar_new();//definimos tool bar
@@ -179,29 +179,27 @@ void VentanaPrincipal(GtkWidget *widget, gpointer data)
   gtk_toolbar_insert(GTK_TOOLBAR(Tool), ToolAcercaDe, -1);
   gtk_toolbar_insert(GTK_TOOLBAR(Tool), Separador, -1);
   gtk_toolbar_insert(GTK_TOOLBAR(Tool), ToolSalir, -1);
-  gtk_box_pack_start (GTK_BOX(Widgets->SOpciones->CH[0]),Tool,FALSE,TRUE,0);//incluimos la toolbar en la caja vertical
+  gtk_box_pack_start (GTK_BOX(CajaGrande),Tool,FALSE,TRUE,0);//incluimos la toolbar en la caja vertical
 
   //Creación de botón "OK"
   botonOK=gtk_button_new_from_stock(GTK_STOCK_OK);//nuevo boton OK
-  g_signal_connect(botonOK,"clicked",G_CALLBACK(CerrarVentana),Widgets);
+  gtk_signal_connect(GTK_OBJECT(botonOK),"clicked",GTK_SIGNAL_FUNC(CerrarVentana),Widgets);
   
   //Organización de elementos gráficos
-  gtk_box_pack_start_defaults (GTK_BOX(CajaGrande),Widgets->SOpciones->CH[0]);
-  gtk_box_pack_start_defaults (GTK_BOX(CajaGrande),Widgets->SOpciones->CH[1]);
-  gtk_box_pack_start_defaults (GTK_BOX(Widgets->SOpciones->CH[1]),botonOK);
+  gtk_box_pack_start_defaults (GTK_BOX(CajaGrande),hbox1);//Caja horizontal a caja grande
+  gtk_box_pack_start_defaults (GTK_BOX(hbox1),botonOK);//boton a caja horizontal
   gtk_signal_connect(GTK_OBJECT(Widgets->SVentanas->VenP),"destroy",G_CALLBACK(CerrarVentana), Widgets);
+  
   gtk_widget_show_all(Widgets->SVentanas->VenP);
 
 }//VentanaPrincipal
 
 void VentanaObjetivo2(GtkWidget *widget, gpointer data)
 {
-  GtkWidget *imagen, *boton, *vbox;
+  GtkWidget *imagen;
   GdkPixbuf *pixbuf; 
   ptrWidgets Widgets=(ptrWidgets)data;
   
-  vbox = gtk_vbox_new(TRUE,5);
-
   Widgets->SVentanas->VenO = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW (Widgets->SVentanas->VenO), "Flujo de Tráfico en Santa Fe");
   gtk_window_set_position(GTK_WINDOW(Widgets->SVentanas->VenO), GTK_WIN_POS_CENTER);
@@ -209,29 +207,23 @@ void VentanaObjetivo2(GtkWidget *widget, gpointer data)
   gtk_window_set_resizable(GTK_WINDOW(Widgets->SVentanas->VenO), FALSE);
   gtk_container_border_width(GTK_CONTAINER(Widgets->SVentanas->VenO),5);
 
+  //Creación de imagen
   pixbuf = gdk_pixbuf_new_from_file_at_scale("Archivos/objective.png", 300, 300, FALSE, NULL);
   imagen = gtk_image_new_from_pixbuf(pixbuf);
-  boton= gtk_button_new();
-  
   gtk_widget_show (imagen);
 
-  gtk_signal_connect(GTK_OBJECT(boton),"clicked",GTK_SIGNAL_FUNC(Esconder), Widgets);
   gtk_signal_connect(GTK_OBJECT(Widgets->SVentanas->VenO),"destroy",G_CALLBACK(Esconder), Widgets);
-  gtk_container_add(GTK_CONTAINER(Widgets->SVentanas->VenO), boton);
-  gtk_container_add(GTK_CONTAINER(boton), vbox);
-  gtk_container_add(GTK_CONTAINER(vbox), GTK_WIDGET(imagen));
+  gtk_container_add(GTK_CONTAINER(Widgets->SVentanas->VenO), imagen);
   
   gtk_widget_show_all(Widgets->SVentanas->VenO);
 }//VentanaObjetivo2
 
 void VentanaInstrucciones2(GtkWidget *widget, gpointer data)
 {
-  GtkWidget *imagen, *boton, *vbox;
+  GtkWidget *imagen;
   GdkPixbuf *pixbuf; 
   ptrWidgets Widgets=(ptrWidgets)data;
   
-  vbox = gtk_vbox_new(TRUE,5);
-
   Widgets->SVentanas->VenI = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW (Widgets->SVentanas->VenI), "Flujo de Tráfico en Santa Fe");
   gtk_window_set_position(GTK_WINDOW(Widgets->SVentanas->VenI), GTK_WIN_POS_CENTER);
@@ -240,16 +232,11 @@ void VentanaInstrucciones2(GtkWidget *widget, gpointer data)
   gtk_container_border_width(GTK_CONTAINER(Widgets->SVentanas->VenI),5);
 
   pixbuf = gdk_pixbuf_new_from_file_at_scale("Archivos/instructions.png", 300, 300, FALSE, NULL);
-  imagen = gtk_image_new_from_pixbuf(pixbuf);
-  boton= gtk_button_new();
-  
+  imagen = gtk_image_new_from_pixbuf(pixbuf);  
   gtk_widget_show (imagen);
 
-  gtk_signal_connect(GTK_OBJECT(boton),"clicked",GTK_SIGNAL_FUNC(Esconder), Widgets);
   gtk_signal_connect(GTK_OBJECT(Widgets->SVentanas->VenI),"destroy",G_CALLBACK(Esconder), Widgets);
-  gtk_container_add(GTK_CONTAINER(Widgets->SVentanas->VenI), boton);
-  gtk_container_add(GTK_CONTAINER(boton), vbox);
-  gtk_container_add(GTK_CONTAINER(vbox), GTK_WIDGET(imagen));
+  gtk_container_add(GTK_CONTAINER(Widgets->SVentanas->VenI), imagen);
   
   gtk_widget_show_all(Widgets->SVentanas->VenI);
 }//VentanaIntrucciones2
